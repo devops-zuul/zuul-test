1f5489cd994261bad02463e5b4d29fb8ab4a4c36
devops-zuul <69584999+devops-zuul@users.noreply.github.com>
2020-10-27 15:38:23 +0530

    Create cal.py

---
+++ playbooks/cal.py
@@ -0,0 +1,31 @@
+def add(first_term, second_term):
+    return first_term + second_term
+
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+
+def multiple(first_term, second_term):
+    return first_term * second_term
+
+
+def divide(first_term, second_term):
+    return first_term / second_term
+
+
+print("calling add...")
+print("result of addition 1 and 2  is =")
+print(add(1,2))
+
+print("\n calling substract....")
+print("result of subtraction 4 and 2  is =")
+print(subtract(4,2))
+
+print("\n calling multiply....")
+print("result of multiply 3500 and 2  is =")
+print(multiple(3500,2))
+
+print("\n calling divide....")
+print("result of divide 1000 and 80  is =")
+print(divide(1000,80))

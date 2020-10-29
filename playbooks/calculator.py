1e45299fad56f147f126295307e89173b71dd5f5
devops-zuul <69584999+devops-zuul@users.noreply.github.com>
2020-10-29 11:56:39 +0530

    Update calculator.py

--- playbooks/calculator.py
+++ playbooks/calculator.py
@@ -1,16 +1,25 @@
+import sys 
 def add(first_term, second_term):
     return first_term + second_term
 
  
-
 def subtract(first_term, second_term):
     return first_term - second_term
 
-print("calling add...")
-print("result of addition 1 and 2  is =")
-print(add(1,2))
 
-print("\n calling substract....")
-print("result of subtraction 4 and 2  is =")
-print(subtract(4,2))
+def print_to_stdout(*a): 
+    # Here a is the array holding the objects 
+    # passed as the arguement of the function 
+    print(*a, file = sys.stdout) 
+
+ 
+
+
+print_to_stdout("calling add...")
+print_to_stdout("result of addition 1 and 2  is =")
+print_to_stdout(add(1,2))
+
+print_to_stdout("\n calling substract....")
+print_to_stdout("result of subtraction 4 and 2  is =")
+print_to_stdout(subtract(4,2))
 

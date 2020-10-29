ba9185970ed6abeb6f62fe501165d7591031e034
devops-zuul <69584999+devops-zuul@users.noreply.github.com>
2020-10-29 14:45:33 +0530

    Create calculator1.py

---
+++ playbooks/calculator1.py
@@ -0,0 +1,15 @@
+def add(first_term, second_term):
+    return first_term + second_term
+
+ 
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+print("calling add...")
+print("result of addition 1 and 2  is =")
+print(add(1,2))
+
+print("\n calling substract....")
+print("result of subtraction 4 and 2  is =")
+print(subtract(4,2))
